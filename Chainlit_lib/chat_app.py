cd63563ab997ce87df6e52333b39179b679eae56
Uttam patel <utampipaliya@gmail.com>
2023-06-19 12:44:25 +0530

    template add

--- Chainlit_lib/chat_app.py
+++ Chainlit_lib/chat_app.py
@@ -11,7 +11,7 @@ HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
 # --------------------------------------------------------------
 
 llm = HuggingFaceHub(
-    repo_id="tiiuae/falcon-7b-instruct", model_kwargs={"temperature": 0.8, "max_new_tokens": 500}
+    repo_id="tiiuae/falcon-7b-instruct", model_kwargs={"temperature": 0.1, "max_new_tokens": 500}
 )
 
 # --------------------------------------------------------------
@@ -19,9 +19,12 @@ llm = HuggingFaceHub(
 
 # Answer: Let's think step by step."""
 
-template = """Question: {question}
+# template = """Question: {question}
+# Answer: """
 
-Answer:"""
+
+template = """Answer the following question: {question}
+"""
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
 llm_chain = LLMChain(prompt=prompt, llm=llm,verbose=True)
@@ -30,6 +33,7 @@ llm_chain = LLMChain(prompt=prompt, llm=llm,verbose=True)
 @cl.on_message
 async def main(message: str):
     # Your custom logic goes here...
+    
     response = llm_chain.run(message)
     wrapped_text = textwrap.fill(
     response, width=100, break_long_words=False, replace_whitespace=False)
@@ -37,4 +41,3 @@ async def main(message: str):
     await cl.Message(
         content=f"{wrapped_text}",
     ).send()
-
